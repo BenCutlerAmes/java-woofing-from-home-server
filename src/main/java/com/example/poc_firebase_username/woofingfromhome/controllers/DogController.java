46c4dd9308c67cb3e5f6a5ed17e79774211e1059
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-11-01 13:12:30 +0000

    added in the dog routes

--- src/main/java/com/example/poc_firebase_username/woofingfromhome/controllers/DogController.java
+++ src/main/java/com/example/poc_firebase_username/woofingfromhome/controllers/DogController.java
@@ -1,4 +1,47 @@
 package com.example.poc_firebase_username.woofingfromhome.controllers;
 
+import com.example.poc_firebase_username.woofingfromhome.models.Customer;
+import com.example.poc_firebase_username.woofingfromhome.models.Dog;
+import com.example.poc_firebase_username.woofingfromhome.repositories.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class DogController {
+
+    @Autowired
+    DogRepository dogRepository;
+
+
+    @GetMapping(value = "/dogs")
+    public ResponseEntity<List<Dog>> getAllCustomers() {
+        return new ResponseEntity<>(dogRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/dogs/{id}")
+    public ResponseEntity getDogs(@PathVariable Long id) {
+        return new ResponseEntity<>(dogRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/dogs")
+    public ResponseEntity<Dog> postDog(@RequestBody Dog dog) {
+        dogRepository.save(dog);
+        return new ResponseEntity<>(dog, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/dogs/{id}")
+    public ResponseEntity<Dog> updateDog(@PathVariable Long id, @RequestBody Dog dog){
+        dogRepository.save(dog);
+        return new ResponseEntity<>(dog, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/dogs/{id}")
+    public ResponseEntity deleteDog(@PathVariable Long id) {
+        dogRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
