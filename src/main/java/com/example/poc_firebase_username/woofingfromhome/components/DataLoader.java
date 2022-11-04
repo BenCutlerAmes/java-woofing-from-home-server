755fd7ed3001de2b317fc29933753c733c991f37
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-11-04 12:11:18 +0100

    some seed data created, not running on server start

--- src/main/java/com/example/poc_firebase_username/woofingfromhome/components/DataLoader.java
+++ src/main/java/com/example/poc_firebase_username/woofingfromhome/components/DataLoader.java
@@ -2,6 +2,7 @@ package com.example.poc_firebase_username.woofingfromhome.components;
 
 import com.example.poc_firebase_username.woofingfromhome.models.Customer;
 import com.example.poc_firebase_username.woofingfromhome.models.Dog;
+import com.example.poc_firebase_username.woofingfromhome.models.Helpers;
 import com.example.poc_firebase_username.woofingfromhome.models.Match;
 import com.example.poc_firebase_username.woofingfromhome.repositories.CustomerRepository;
 import com.example.poc_firebase_username.woofingfromhome.repositories.DogRepository;
@@ -12,8 +13,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.math.MathContext;
+
 @Profile("!test") //Run every time EXCEPT Tests
-//@Component
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
@@ -25,54 +28,66 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     MatchRepository matchRepository;
 
-    public DataLoader() {}
+    public DataLoader() {
+    }
 
     public void run(ApplicationArguments args) {
-        Customer customer = new Customer("Conrad", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6, "33.33", "33.33");
-        customerRepository.save(customer);
+//        Customer ben = new Customer("Ben", true, true, true, false, false, false, false, false, false, false, false, true, false, false, false, true, false, 2, false, false, 3, "-3.1351147", "55.901599");
+//        customerRepository.save(ben);
+//
+//        Customer conrad = new Customer("Conrad", true, false, false, false, true, false, false, false, true, true, true, false, false, false, false, true, false, 3, false, true, 3, "-3.1352243", "55.902431");
+//        customerRepository.save(conrad);
+//
+//        Customer callum = new Customer("Callum", false, true, true, false, true, false, false, true, false, false, true, false, false, false, false, true, false, 3, false, false, 3, "-3.129965", "55.100000");
+//        customerRepository.save(callum);
+//
+//        Customer katie = new Customer("Katie", true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 3, false, false, 1, "-3.131012", "55.956403");
+//        customerRepository.save(katie);
+//
+//        Dog saffy = new Dog("Saffy", "9-12-2016", "Old English Sheepdog", 3, 2, true, false, true, true, true, ben);
+//        dogRepository.save(saffy);
+//
+//        Dog teddy = new Dog("Teddy", "14-3-2018", "Golden Retriever", 2, 3, false, true, true, true, true, callum);
+//        dogRepository.save(teddy);
+//
+//        Dog flo = new Dog("Flo", "4-07-2020", "Cavapoo", 1, 1, true, false, true, false, true, conrad);
+//        dogRepository.save(flo);
 
-        Customer customer2 = new Customer("Callum", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6, "33.33", "33.33");
-        customerRepository.save(customer2);
-
-
-        Customer customer3 = new Customer("Katie", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6, "33.33", "33.33");
-        customerRepository.save(customer3);
-
-//        Customer customer2 = new Customer("changed1234", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6);
-//        customerRepository.save(customer2);
+//        Match match1 = new Match(ben, callum, Helpers.calculateDistanceFromAPI(ben, callum), Helpers.calculateMatchScore(ben, callum));
+//        matchRepository.save(match1);
 //
-//        Customer customer3 = new Customer("newPerson3", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6);
-//        customerRepository.save(customer3);
-//
-//        Customer customer4 = new Customer("newPerson4", true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6);
-//        customerRepository.save(customer4);
-//
-//        Customer customer5 = new Customer("newPerson5",true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6);
-//        customerRepository.save(customer5);
-//
-//        Customer customer6 = new Customer("newPerson6",true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,true,9,true,true,6);
-//        customerRepository.save(customer6);
-//
-        Dog dog = new Dog("Bingo","7-12-2021","Cavapoo",3,3,true,true,true,true,false,customer);
-        dogRepository.save(dog);
-//
-        Match match = new Match(customer, customer2, 1234, 98.3);
-        matchRepository.save(match);
-
-        Match match1 = new Match(customer, customer3, 99, 99);
-        matchRepository.save(match1);
-
-        Match match21 = new Match(customer2, customer, 99, 99);
-        matchRepository.save(match21);
-//
-//        Match match2 = new Match(customer2, customer, 8454, 100);
+//        Match match2 = new Match(ben, conrad, Helpers.calculateDistanceFromAPI(ben, conrad), Helpers.calculateMatchScore(ben, conrad));
 //        matchRepository.save(match2);
 //
-//        Match match3 = new Match(customer3, customer4, 9342, 13);
+//        Match match3 = new Match(ben, katie, Helpers.calculateDistanceFromAPI(ben, katie), Helpers.calculateMatchScore(ben, katie));
 //        matchRepository.save(match3);
 //
-//        Match match4 = new Match(customer5, customer6, 9641, 35);
+//        Match match4 = new Match(callum, ben, Helpers.calculateDistanceFromAPI(callum, ben), Helpers.calculateMatchScore(callum, ben));
 //        matchRepository.save(match4);
+//
+//        Match match5 = new Match(callum, conrad, Helpers.calculateDistanceFromAPI(callum, conrad), Helpers.calculateMatchScore(callum, conrad));
+//        matchRepository.save(match5);
+//
+//        Match match6 = new Match(callum, katie, Helpers.calculateDistanceFromAPI(callum, katie), Helpers.calculateMatchScore(callum, katie));
+//        matchRepository.save(match6);
+//
+//        Match match7 = new Match(conrad, ben, Helpers.calculateDistanceFromAPI(conrad, ben), Helpers.calculateMatchScore(conrad, ben));
+//        matchRepository.save(match7);
+//
+//        Match match8 = new Match(conrad, callum, Helpers.calculateDistanceFromAPI(conrad, callum), Helpers.calculateMatchScore(conrad, callum));
+//        matchRepository.save(match8);
+//
+//        Match match9 = new Match(conrad, katie, Helpers.calculateDistanceFromAPI(conrad, katie), Helpers.calculateMatchScore(conrad, katie));
+//        matchRepository.save(match9);
+//
+//        Match match10 = new Match(katie, ben, Helpers.calculateDistanceFromAPI(katie, ben), Helpers.calculateMatchScore(katie, ben));
+//        matchRepository.save(match10);
+//
+//        Match match11 = new Match(katie, callum, Helpers.calculateDistanceFromAPI(katie, callum), Helpers.calculateMatchScore(katie, callum));
+//        matchRepository.save(match11);
+//
+//        Match match12 = new Match(katie, conrad, Helpers.calculateDistanceFromAPI(katie, conrad), Helpers.calculateMatchScore(katie, conrad));
+//        matchRepository.save(match12);
 
 
     }
