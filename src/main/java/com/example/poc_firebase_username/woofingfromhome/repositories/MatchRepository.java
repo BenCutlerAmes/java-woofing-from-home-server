fad09f62a3ba9264c7c17132fed9f709cfdd49b2
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-11-07 16:12:41 +0000

    query updated

--- src/main/java/com/example/poc_firebase_username/woofingfromhome/repositories/MatchRepository.java
+++ src/main/java/com/example/poc_firebase_username/woofingfromhome/repositories/MatchRepository.java
@@ -3,6 +3,7 @@ package com.example.poc_firebase_username.woofingfromhome.repositories;
 import com.example.poc_firebase_username.woofingfromhome.models.Dog;
 import com.example.poc_firebase_username.woofingfromhome.models.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
-    List<Match> findByCustomer1FirebaseIdOrCustomer2FirebaseId(String firebaseId,String firebaseId2 );
+    List<Match> findByCustomer1_FirebaseIdAndScoreGreaterThanOrCustomer2_FirebaseId(String firebaseId, double score, String firebaseId1);
+
 }
